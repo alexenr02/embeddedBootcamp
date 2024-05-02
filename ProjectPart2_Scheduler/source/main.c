7b3ca7a94df73e2fbd9f2ba7b3ddd7ab22896c21
Alex Enriquez <aelejandro9@gmail.com>
2024-05-01 23:36:11 -0600

    implement scheduler

--- ProjectPart2_Scheduler/source/main.c
+++ ProjectPart2_Scheduler/source/main.c
@@ -16,24 +16,24 @@ void Task_500ms(void);
 void Task_1000ms(void);
 
 int main( void )
-{/*
+{
     unsigned char TaskID1;
     unsigned char TaskID2;
 
-    /* init the scheduler with two tasks and a tick time of 100ms and run for 10 seconds only 
+    /* init the scheduler with two tasks and a tick time of 100ms and run for 10 seconds only */ 
     Sche.tick = TICK_VAL;
     Sche.tasks = TASKS_N;
     Sche.timeout = 10000;
     Sche.taskPtr = tasks;
     Sched_initScheduler ( &Sche );
 
-    /* register two task with their corresponding init functions and their periodicity, 100ms and 500ms 
+    /* register two task with their corresponding init functions and their periodicity, 100ms and 500ms*/ 
     TaskID1 = Sched_registerTask( &Sche, Init_500ms, Task_500ms, 500 );
     TaskID2 = Sched_registerTask( &Sche, Init_1000ms, Task_1000ms, 1000);
 
-    /* run the scheduler for the amount of time stablished in Sche.timeout 
+    /* run the scheduler for the amount of time stablished in Sche.timeout */
     Sched_startScheduler( &Sche );
-    */
+    
     return 0;
     
 }
